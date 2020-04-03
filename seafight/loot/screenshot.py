49f98cb056fd0afadbbda6c1cb148e4372797b6f
sofiane af <sofianeaffes@gmail.com>
2020-04-03 23:31:46 +0200

    update loop

--- seafight/loot/screenshot.py
+++ seafight/loot/screenshot.py
@@ -1,4 +1,3 @@
-import pyscreenshot as ImageGrab
 import pyautogui
 from time import sleep
 
@@ -11,7 +10,7 @@ print(width,height)
 btn_attack = 'q'
 btn_cancel_attack = 'w'
 btn_toggle_opponents = 'r'
-btn_set_corse = 't'
+btn_set_course = 't'
 btn_to_the_ship = 'z'
 btn_repair = 'u'
 
@@ -21,38 +20,61 @@ b_y1 = 929
 b_x2 = 1201
 b_y2 = 972
 
-while(1):
-    # waiting for a target
+def loot():
+    j = 0
     while(1):
-        pyautogui.typewrite([btn_toggle_opponents, btn_attack], interval=1)
-        a = pyautogui.locateOnScreen('full_img/stop_active.png', grayscale=True)
-        if a != None:
-            break
-        sleep(1.5)
-    print("found target")
+        j = j + 1
+        # waiting for a target
+        i = 0
+        while(1):
+            i = i + 1
+            pyautogui.typewrite([btn_toggle_opponents, btn_attack], interval=1)
+            a = pyautogui.locateOnScreen('full_img/asus/stop_active.png', grayscale=True)
+            if a != None:
+                #stop_ship()
+                break
+            print(i,j)
+            if i == 4:
+                #sale_ship(j % 2)
+                j = j + 1
+            sleep(1)
+        print("found target")
 
-    # kill target
-    while(1):
-        a = pyautogui.locateOnScreen('full_img/stop_active.png', grayscale=True)
+        # kill target
+        while(1):
+            a = pyautogui.locateOnScreen('full_img/asus/stop_active.png', grayscale=True)
+            if a == None:
+                break
+            sleep(10)
+        print("kill done")
+
+        # repair if needed
+        sleep(1)
+        a = pyautogui.locateOnScreen('full_img/asus/repair_inactive.png', grayscale=True)
         if a == None:
-            break
-        sleep(10)
-    print("kill done")
+            pyautogui.typewrite([btn_repair], interval=1)
+            print("repair")
 
-    # repair if needed
-    sleep(20)
-    a = pyautogui.locateOnScreen('full_img/repair_inactive.png', grayscale=True)
-    if a == None:
-        pyautogui.typewrite([btn_repair], interval=1)
-        print("repair")
+        # wait till repear is done
+        while(1):
+            sleep(10)
+            a = pyautogui.locateOnScreen('full_img/asus/repair_inactive.png', grayscale=True)
+            if a != None:
+                print("repear done")
+                break
 
-    # wait till repear is done
-    while(1):
-        sleep(10)
-        a = pyautogui.locateOnScreen('full_img/repair_inactive.png', grayscale=True)
-        if a != None:
-            print("repear done")
-            break
+def sale_ship(direction):
+    if direction == 0:
+        x = 972
+        y = 258
+    elif direction == 1:
+        x = 1049
+        y = 258
+    pyautogui.click(x, y,interval=0.5)
+    pyautogui.typewrite([btn_set_course, btn_to_the_ship], interval=0.5)
 
-#img = ImageGrab.grab(bbox=(b_x1,b_y1,b_x2,b_y2))
-#img.show()
+def stop_ship():
+    pyautogui.typewrite([btn_to_the_ship, 'up', btn_set_course], interval=0.1)
+
+#loot()qr
+loot()
